ca10b66a479b33e800284ee6b0c628f34b4ae2b1
Nathan Caetano <nathancaetano599@gmail.com>
2023-05-18 17:57:54 -0300

    subindo projeto

---
+++ src/components/tarefa.vue
@@ -0,0 +1,33 @@
+<template>
+<box>
+    <div class="columns">
+        <div class="column is-7">{{tarefa.descricao || 'Tarefa sem descrição' }}</div>
+        <div class="column">
+            <cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+        </div>
+    </div>
+</box>
+</template>
+
+<script lang="ts">
+
+import { defineComponent, PropType } from 'vue';
+import cronometro from "./cronometro.vue";
+import iTarefa from '@/interface/iTarefa';
+import box from './box.vue';
+
+export default defineComponent({
+    name:"tarefa-",
+    components:{
+        cronometro,
+        box
+    },
+    props:{
+        tarefa:{
+            type: Object as PropType<iTarefa>,
+                required: true
+        }
+    }
+});
+</script>
+
